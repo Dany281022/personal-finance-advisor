2fbe7b24315e337750e5c4e17c0dc075443c947b
Dany <a00316024@mycambrian.ca>
2026-04-19 08:58:28 -0400

    Fix: pin AWS provider to 5.50.0, commit lock file, update Lambda

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.6"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = { source = "hashicorp/aws", version = "= 5.50.0" }
   }
   backend "s3" {
     bucket = "personal-finance-advisor-tfstate-352956325292"
